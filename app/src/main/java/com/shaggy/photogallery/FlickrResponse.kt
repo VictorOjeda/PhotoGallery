c05da4b00dc12b4f0c3740dcce2c9ed334d212ab
VictorOjeda <victorm.ojedav@gmail.com>
2020-08-23 20:08:38 -0700

    First commit

---
+++ app/src/main/java/com/shaggy/photogallery/FlickrResponse.kt
@@ -0,0 +1,5 @@
+package com.shaggy.photogallery
+
+class FlickrResponse {
+    lateinit var photos: PhotoResponse
+}
---
+++ app/src/main/java/com/shaggy/photogallery/PhotoGalleryViewModel.kt
@@ -0,0 +1,34 @@
+package com.shaggy.photogallery
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.shaggy.photogallery.api.FlickrFetchr
+
+class PhotoGalleryViewModel(private val app: Application) : AndroidViewModel(app) {
+    val galleryItemLiveData: LiveData<List<GalleryItem>>
+
+    private val flickrFetchr = FlickrFetchr()
+    private val mutableSearchTerm = MutableLiveData<String>()
+    val searchTerm: String
+        get() = mutableSearchTerm.value ?: ""
+
+    init {
+        mutableSearchTerm.value = QueryPreferences.getStoredQuery(app)
+        galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) { searchTerm ->
+            if (searchTerm.isBlank()) {
+                flickrFetchr.fetchPhotos()
+            } else {
+                flickrFetchr.searchPhotos(searchTerm)
+            }
+        }
+    }
+
+    fun fetchPhotos(query: String = "") {
+        QueryPreferences.setStoredQuery(app, query)
+        mutableSearchTerm.value = query
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
